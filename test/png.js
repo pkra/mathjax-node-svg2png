82dea0a8e04369f467be1d700b8e7d49a1a65eae
Thomas Hufschmidt <hufschmidt@hrz.uni-marburg.de>
2017-09-22 19:06:30 +0200

    Added check to ensure optionas parameter is not modified

--- test/png.js
+++ test/png.js
@@ -1,10 +1,11 @@
 var tape = require('tape');
 var typeset = require("../lib/main.js").typeset;
 tape('basic test: check PNG generation', function(t) {
-    t.plan(2);
-    var options = {math: '\\sin(x)', format:'TeX', png:true};
+    t.plan(3);
+    var options = {math: '\\sin(x)', format:'TeX', svg: false, png:true};
     typeset(options, function (result) {
         t.equal(result.png.indexOf("data:image/png;base64"), 0, 'PNG data-uri header');
         t.ok(result.png.length > 100, 'PNG data length');
+        t.equal(options.svg, false, 'Invariant options');
     });
 });
